c3b37407e7710d6c5d6cd23486542b4e92e9b668
varit05 <varitpatel@gmail.com>
2020-04-11 14:50:42 +0530

    Updates

---
+++ precache-manifest.9006d97c7df11c7580a215e41149644e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2709321ecf728069a31ab7f03581cdab",
+    "url": "/covid19-usa/index.html"
+  },
+  {
+    "revision": "67d4b19e4be53d4d41bc",
+    "url": "/covid19-usa/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e997188136db3539c26e",
+    "url": "/covid19-usa/static/js/2.ca756d75.chunk.js"
+  },
+  {
+    "revision": "b4610c35fd5e6efa7493e375f2f8ce97",
+    "url": "/covid19-usa/static/js/2.ca756d75.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67d4b19e4be53d4d41bc",
+    "url": "/covid19-usa/static/js/main.aa74cac3.chunk.js"
+  },
+  {
+    "revision": "87fc00b41228daf62141",
+    "url": "/covid19-usa/static/js/runtime-main.a1cbf5d2.js"
+  }
+]);
